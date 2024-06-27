4839f228ee189da282c32a2480f1c9319baa7957
drumundana <96789004+drumundana@users.noreply.github.com>
2024-06-27 19:14:04 +1000

    Home page

--- home.php
+++ home.php
@@ -1,41 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="style/style.css" rel="stylesheet">
-        <link href="style/bootstrap.min.css" rel="stylesheet">
-        <title>Home</title>
-    </head>
-    <body class="home">
-        <header id="header">
-            <nav>
-               <a><img src="images\logo_green.png" alt="logo" class="logo"></a>
-               <ul>
-                <li><a href="home.php">Home</a></li>
-                <li><a href="index.php">Menu</a></li>
-                <li><a href="order.php">Order</a></li>
-                <li><a href="User_login_page.php">Login</a></li>
-               </ul> 
-            </nav>
-        </header>
-        <section>
-            <div class="content">
-                <img src="images\botton.png" alt="donuts" class="image-donuts">
-                    <div class="text">
-                    <h3>"The search for indulgence is over; Brisa Donuts is on!"</h3>
-                    </div>
-            </div>
-        </section>
-        <section>
-          <div>
-            <h1>Let us Cater!</h1>
-            <h3>Do you have an event coming up? Want to replace your wedding cake with a towering stack of doughnuts? Or maybe you want to blow your clients away with the perfect corporate gift? Whatever the occasion, we’ve got you covered!</h3>
-          </div>
-        </section>
-      <footer class="footer">              
-      <p>&copy; 2023 Brisa Donuts. All rights reserved.</p>
-      <p>contato@brisadonuts.com</p>
-      </footer>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+  <header class="header">
+    <nav class="navbar">
+      <a href="home.php">Home</a>
+      <a href="#">Cart</a>
+      <a href="Costumer_login.php">Login</a>
+    </nav>
+    <form action="#" class="search-bar">
+      <input type="text" placeholder="Search...">
+      <button type="submit"><ion-icon name="search-circle-outline"></ion-icon></button>
+    </form>
+  </header>
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
