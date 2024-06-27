a50ca408b9d2ef2355deaf15b03ef330d5d3f302
drumundana <96789004+drumundana@users.noreply.github.com>
2024-06-27 11:20:41 +1000

    login2

--- User_login.php
+++ User_login.php
@@ -2,22 +2,27 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Login</title>
-    <link rel="stylesheet" href="style/styles.css">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
-    <div class="login-dialog">
-        <div class="dialog-title-bar">
-            <span class="dialog-title">User Login</span>
-        </div>
-        <form class="login-form">
-            <label for="username">User Name</label>
-            <input type="text" id="username" name="username" required>
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" required>
-            <button type="submit">Login</button>
+    <div class="login-box">
+        <form action="">
+            <h2>User Login</h2>
+            <div class="input-box">
+                <span class="icon"><ion-icon name="person-circle-outline"></ion-icon></span>
+                <input type="username" required>
+                <label>Username</label>
+            </div>
+            <div class="input-box">
+                <span class="icon"><ion-icon name="lock-closed-outline"></ion-icon></span>
+                <input type="password" required>
+                <label>Password</label>
+            </div>
         </form>
-    </div>
+        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
