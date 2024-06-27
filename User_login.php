27f97813b33458f2be6b49f7cce0d6705fa80b63
drumundana <96789004+drumundana@users.noreply.github.com>
2024-06-27 18:37:51 +1000

    register link

--- User_login.php
+++ User_login.php
@@ -27,8 +27,8 @@
                 <a href="#">Forgot Password?</a>
             </div>
             <button type="submit">Login</button>
-            <div class="register-link">
-                <p>Don't have an account? <a heref="user_register.php">Register</a></p>
+            <div class="user_register.php">
+                <p>Don't have an account? <a heref="#">Register</a></p>
             </div>
         </form>
         <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
