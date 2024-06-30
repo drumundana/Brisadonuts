75b3ef0eb55da207f3128feb08983aea5b718f59
drumundana <96789004+drumundana@users.noreply.github.com>
2024-06-30 18:52:44 +1000

    main

---
+++ Main.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+  <header class="header">
+    <nav class="navbar">
+      <a href="home.php">Home</a>
+      <a href="#">Cart</a>
+      <a href="Costumer_login.php">Login</a>
+    </nav>
+    <form action="#" class="search-bar">
+      <input type="text" placeholder="Search...">
+      <button type="submit"><ion-icon name="search-outline"></ion-icon></button>
+    </form>
+  </header>
+
+  <footer>
+      <p>&copy; 2024 LPA eComms. All rights reserved.</p>
+      <nav>
+        <a href="Main.php">Admin</a>
+      </nav>
+  </footer>
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
